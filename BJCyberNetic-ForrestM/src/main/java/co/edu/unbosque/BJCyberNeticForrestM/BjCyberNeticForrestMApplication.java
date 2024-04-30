c0916e2a659c522548586a9206c1a19e8056b71e
BrajhanM <Camilo>
2024-04-29 21:10:24 -0500

    Json

---
+++ BJCyberNetic-ForrestM/src/main/java/co/edu/unbosque/BJCyberNeticForrestM/BjCyberNeticForrestMApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.BJCyberNeticForrestM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjCyberNeticForrestMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjCyberNeticForrestMApplication.class, args);
+	}
+
+}
---
+++ BJCyberNetic-ForrestM/src/test/java/co/edu/unbosque/BJCyberNeticForrestM/BjCyberNeticForrestMApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.BJCyberNeticForrestM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjCyberNeticForrestMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
